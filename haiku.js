01480336852ebe188ab352f854da8dac1208c921
Mick Negenman <36791@ma-web.nl>
2023-10-09 10:13:03 +0200

    Haiku 9

--- haiku.js
+++ haiku.js
@@ -1,54 +1,58 @@
-// Plak hier de haiku functions onder elkaar
 const haikus = [
-    () => {
+    function Haiku1() {
         console.log("Haiku by:", "Kobayahsi Issa");
         console.log("The wren");
         console.log("Earns his living");
         console.log("Noiselessly");
     },
-    () => {
+    function Haiku2() {
         console.log("Haiku by:", "Matsuo Bashō");
         console.log("From time to time");
         console.log("The clouds give rest");
         console.log("To the moon-beholders.");
     },
-    () => {
+    function Haiku3() {
         console.log("Haiku by:", "Yosa Buson");
         console.log("Over-ripe sushi,");
         console.log("The Master");
         console.log("Is full of regret.");
     },
-    () => {
+    function Haiku4() {
         console.log("Haiku by:", "Masaoaka Shiki");
         console.log("Consider me");
         console.log("As one who loved poetry");
         console.log("And persimmons.");
     },
-    () => {
+    function Haiku5() {
         console.log("Haiku by:", "Matsuo Bashō");
         console.log("In the cicada's cry");
         console.log("No sign can foretell");
         console.log("How soon it must die.");
     },
-    () => {
+    function Haiku6() {
         console.log("Haiku by:", "Yosa Buson");
         console.log("Blowing from the west");
         console.log("Fallen leaves gather");
         console.log("In the east.");
     },
-    () => {
+    function Haiku7() {
         console.log("Haiku by:", "Kobayashi Issa");
         console.log("Winter seclusion -");
         console.log("Listening, that evening,");
         console.log("To the rain in the mountain.");
-    }.
-    () => {
+    },
+    function Haiku8() {
         console.log("Haiku by:", "Kobayashi Issa");
         console.log("Don’t weep, insects -");
         console.log("Lovers, stars themselves,");
         console.log("Must part.");
+    },
+    function Haiku9() {
+        console.log("Haiku by:", "- Masaoka Shiki");
+        console.log("My life, -");
+        console.log("How much more of it remains?");
+        console.log("The night is brief.");
     }
 ];
 
-// Laat de code hieronder met rust
 haikus.forEach(haiku => haiku());

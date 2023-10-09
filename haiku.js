c97902d7c4bc1c142e1db015486b7087c9e0ec43
Mick Negenman <36791@ma-web.nl>
2023-10-09 10:06:42 +0200

    Improvements

--- haiku.js
+++ haiku.js
@@ -1,58 +1,48 @@
 // Plak hier de haiku functions onder elkaar
-function Haiku1() {
-    console.log("Haiku by:", "Kobayahsi Issa");
-    console.log("The wren");
-    console.log("Earns his living");
-    console.log("Noiselessly");
-}
-
-function Haiku2() {
-    console.log("Haiku by:", "Matsuo Bashō");
-    console.log("From time to time");
-    console.log("The clouds give rest");
-    console.log("To the moon-beholders.");
-}
-
-function Haiku3() {
-    console.log("Haiku by:", "Yosa Buson");
-    console.log("Over-ripe sushi,");
-    console.log("The Master");
-    console.log("Is full of regret.");
-}
-
-function Haiku4() {
-    console.log("Haiku by:", "Masaoaka Shiki");
-    console.log("Consider me");
-    console.log("As one who loved poetry");
-    console.log("And persimmons.");
-}
-
-function Haiku5() {
-    console.log("Haiku by:", "Matsuo Bashō");
-    console.log("In the cicada's cry");
-    console.log("No sign can foretell");
-    console.log("How soon it must die.");
-}
-
-function Haiku6() {
-    console.log("Haiku by:", "Yosa Buson");
-    console.log("Blowing from the west");
-    console.log("Fallen leaves gather");
-    console.log("In the east.");
-}
-
-function Haiku7() {
-    console.log("Haiku by:", "Kobayashi Issa");
-    console.log("Winter seclusion -");
-    console.log("Listening, that evening,");
-    console.log("To the rain in the mountain.");
-}
+const haikus = [
+    () => {
+        console.log("Haiku by:", "Kobayahsi Issa");
+        console.log("The wren");
+        console.log("Earns his living");
+        console.log("Noiselessly");
+    },
+    () => {
+        console.log("Haiku by:", "Matsuo Bashō");
+        console.log("From time to time");
+        console.log("The clouds give rest");
+        console.log("To the moon-beholders.");
+    },
+    () => {
+        console.log("Haiku by:", "Yosa Buson");
+        console.log("Over-ripe sushi,");
+        console.log("The Master");
+        console.log("Is full of regret.");
+    },
+    () => {
+        console.log("Haiku by:", "Masaoaka Shiki");
+        console.log("Consider me");
+        console.log("As one who loved poetry");
+        console.log("And persimmons.");
+    },
+    () => {
+        console.log("Haiku by:", "Matsuo Bashō");
+        console.log("In the cicada's cry");
+        console.log("No sign can foretell");
+        console.log("How soon it must die.");
+    },
+    () => {
+        console.log("Haiku by:", "Yosa Buson");
+        console.log("Blowing from the west");
+        console.log("Fallen leaves gather");
+        console.log("In the east.");
+    },
+    () => {
+        console.log("Haiku by:", "Kobayashi Issa");
+        console.log("Winter seclusion -");
+        console.log("Listening, that evening,");
+        console.log("To the rain in the mountain.");
+    }
+];
 
 // Laat de code hieronder met rust
-Haiku1();
-Haiku2();
-Haiku3();
-Haiku4();
-Haiku5();
-Haiku6();
-Haiku7();
+haikus.forEach(haiku => haiku());
